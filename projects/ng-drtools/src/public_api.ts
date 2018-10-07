e815ed423e1f7c560b703ce887c6e61454483d03
DAEMonRaco <daemonraco@gmail.com>
2018-10-07 20:19:13 -0300

    Exports
    * Bug fixes.

--- projects/ng-drtools/src/public_api.ts
+++ projects/ng-drtools/src/public_api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of ng-drtools
  */
 
-export * from './lib/drtools.service';
-export * from './lib/drtools.service/mysql.connector';
-export * from './lib/drtools.service/types';
-export * from './lib/ng-drtools.module';
+export { DRToolsModule } from './lib/ng-drtools.module';
+export { DRToolsService } from './lib/drtools.service';
+export { DRToolsServiceConfig } from './lib/drtools.service/types';
+export { DRToolsServiceMySQL } from './lib/drtools.service/mysql.connector';
