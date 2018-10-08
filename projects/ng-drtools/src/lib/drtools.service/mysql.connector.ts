f9e2842d81c097bcc91f4eb90d3ce1f72fb41042
DAEMonRaco <daemonraco@gmail.com>
2018-10-07 22:40:08 -0300

    MySQL
    * Adding a wait to get the amount of entries.

--- projects/ng-drtools/src/lib/drtools.service/mysql.connector.ts
+++ projects/ng-drtools/src/lib/drtools.service/mysql.connector.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { DRToolsServiceConfig } from './types';
 
@@ -20,23 +21,35 @@ export class DRToolsServiceMySQL {
     }
     //
     // Public methods.
-    public all(query: any = null, limit: number = 25, offset: number = 0, order: any = null): any {
-        let queryParam = '';
+    public all(query: any = null, limit: number = 25, offset: number = 0, order: any = null): Observable<any> {
+        let queryParam: string = this.queryForList(query, limit, offset, order);
+        return this.http.get(`${this.url}?limit=${limit}&offset=${offset}${queryParam}`);
+    }
+    public create(data: any): Observable<any> {
+        return this.http.post(`${this.url}`, data);
+    }
+    public count(query: any = null): Observable<any> {
+        let queryParam: string = this.queryForList(query);
+        return this.http.get(`${this.url}/$count?${queryParam}`);
+    }
+    public get(id: any): Observable<any> {
+        return this.http.get(`${this.url}/${id}`);
+    }
+    public update(id: any, data: any): Observable<any> {
+        return this.http.put(`${this.url}/${id}`, data);
+    }
+    //
+    // Protected methods.
+    protected queryForList(query: any = null, limit: number = 25, offset: number = 0, order: any = null): string {
+        let queryParam: string = '';
+
         if (query && typeof query === 'object' && !Array.isArray(query) && Object.keys(query).length > 0) {
             queryParam += `&query=${encodeURI(JSON.stringify(query))}`;
         }
         if (order && typeof order === 'object' && !Array.isArray(order) && Object.keys(order).length > 0) {
             queryParam += `&order=${encodeURI(JSON.stringify(order))}`;
         }
-        return this.http.get(`${this.url}?limit=${limit}&offset=${offset}${queryParam}`);
-    }
-    public create(data: any): any {
-        return this.http.post(`${this.url}`, data);
-    }
-    public get(id: any): any {
-        return this.http.get(`${this.url}/${id}`);
-    }
-    public update(id: any, data: any): any {
-        return this.http.put(`${this.url}/${id}`, data);
+
+        return queryParam;
     }
 }
